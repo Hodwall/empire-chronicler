886aadcb6371565cd33c10faea120d6bd909c9f7
Suga <okamisuga@gmail.com>
2024-03-15 14:05:05 +0100

    Adds filter button component

--- src/Components/Section/Section.tsx
+++ src/Components/Section/Section.tsx
@@ -1,9 +1,12 @@
 import './Section.css';
 import Weapons from '../../Pages/Weapons/Weapons.tsx';
+import Filterbutton from '../Filterbutton/Filterbutton.tsx';
 
 const Section = () => {
+
   return (
     <div className="Section">
+      <Filterbutton />
       <Weapons />
     </div>
   );
